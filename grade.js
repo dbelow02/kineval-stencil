edf9ede95d061d152b055e804ddca7fd65153a06
Liz Olson <lizolson@umich.edu>
2023-01-17 22:58:25 -0500

    Update grades at time: 17-01-2023-22-52-00

---
+++ grade.js
@@ -0,0 +1,181 @@
+//CI grader generates report at 17-01-2023-21-38-52
+grade = 
+{
+  "dbelow": {
+    "name": "Below, Donovan",
+    "unique_name": "dbelow",
+    "email": "dbelow@umich.edu",
+    "course": "ROB 320/EECS 367 W23",
+    "repository": "dbelow02/kineval-stencil",
+    "assignments": {
+      "Honor": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "ROS Pub/Sub": {
+        "status": "PENDING",
+        "comments": [
+          "Passing 0 of 3 tests"
+        ]
+      },
+      "PathPlan_Heap": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "PathPlan_AStar": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_Euler": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_VelocityVerlet": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pendularm_PID": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_MatrixRoutines": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_Transforms": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_JointRendering": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_Quaternion": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_RexArm": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_BaseControl": "FSMDance_BaseControl",
+      "FSMDance_SetpointControl": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FSMDance_FSM": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_Jacobian": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_JTranspose": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "IK_JPseudoInverse": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "MotionPlan_Collision": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "MotionPlan_2DRRTConnect": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "MotionPlan_CSpaceRRTConnect": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "Pitch": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      },
+      "FK_BaseOffset": {
+        "status": "PENDING",
+        "comments": [
+          ""
+        ]
+      }
+    },
+    "quizzes": [
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      },
+      {
+        "status": "PENDING"
+      }
+    ]
+  }
+}
