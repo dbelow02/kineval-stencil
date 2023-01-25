8b2f80c2744668dab84c34994ea853d1a32775df
Liz Olson <lizolson@umich.edu>
2023-01-24 23:04:50 -0500

    Update grades at time: 24-01-2023-22-53-43

--- grade.js
+++ grade.js
@@ -1,4 +1,4 @@
-//CI grader generates report at 21-01-2023-02-36-07
+//CI grader generates report at 24-01-2023-22-18-27
 grade = 
 {
   "dbelow": {
@@ -21,14 +21,78 @@ grade =
       "PathPlan_Heap": {
         "status": "PENDING",
         "comments": [
-          ""
-        ]
+          "0 out of 10 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 7614 3482 5166 8693 6465 28 5256 5677 3222 4224 345 3836 6678 7879 6072 8862 7295 8850 8000 5541 \\n'"
+          },
+          "Test 1": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 6058 4394 344 726 3407 862 958 6043 8129 3816 5299 5374 9065 3744 6876 2679 4931 5819 1012 692 \\n'"
+          },
+          "Test 2": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 9725 9688 246 4304 3166 1064 5294 8176 8571 5988 6314 4935 7117 1001 5839 5076 619 8973 452 6786 \\n'"
+          },
+          "Test 3": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 2265 1950 1837 8417 3557 4003 4553 875 3197 9737 4773 8634 4355 1974 4065 1363 442 6555 6780 1297 \\n'"
+          },
+          "Test 4": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 8545 5368 8436 1366 1391 3331 5646 6941 2216 7629 4375 3210 7665 9976 799 8481 877 1837 2342 6280 \\n'"
+          },
+          "Test 5": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 2226 8799 9000 2545 1587 71 9280 1998 7732 3646 1653 1586 7423 8540 9918 6506 5794 8943 5118 3189 \\n'"
+          },
+          "Test 6": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 4423 4722 1988 4880 2843 6310 5588 937 5613 97 8329 9822 679 3508 730 964 1165 2080 1471 6632 \\n'"
+          },
+          "Test 7": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 7996 6088 8724 7273 3308 5285 3145 4383 2564 2825 6580 706 3109 9592 2648 3905 7959 9628 6373 5292 \\n'"
+          },
+          "Test 8": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 2993 1357 8178 261 4930 1101 1528 5211 6514 1511 4836 8211 4562 5712 4337 2265 551 9591 307 9577 \\n'"
+          },
+          "Test 9": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 8133 5476 7061 3537 5362 150 2049 902 6313 178 4182 1920 6432 6053 5907 709 5444 203 9666 8295 \\n'"
+          }
+        }
       },
       "PathPlan_AStar": {
         "status": "PENDING",
         "comments": [
-          ""
-        ]
+          "0 out of 5 test cases passed"
+        ],
+        "test results": {
+          "Test 0": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          },
+          "Test 1": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          },
+          "Test 2": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          },
+          "Test 3": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          },
+          "Test 4": {
+            "Status": "Failed",
+            "Stderr": "b'[ERROR]: Search time out\\n'"
+          }
+        }
       },
       "Pendularm_Euler": {
         "status": "PENDING",
