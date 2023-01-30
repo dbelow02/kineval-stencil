0d96e2dbf174e62a006f6b2322a125bd04a7d411
Liz Olson <lizolson@umich.edu>
2023-01-30 13:49:59 -0500

    Update grades at time: 30-01-2023-13-41-20

--- grade.js
+++ grade.js
@@ -1,4 +1,4 @@
-//CI grader generates report at 30-01-2023-11-46-59
+//CI grader generates report at 30-01-2023-13-34-01
 grade = 
 {
   "dbelow": {
@@ -19,78 +19,78 @@ grade =
         "comments": "Passing 0 out of 3 tests. Can get partial credit after due date"
       },
       "PathPlan_Heap": {
-        "status": "PENDING",
+        "status": "PASS",
         "comments": [
-          "0 out of 10 test cases passed"
+          "10 out of 10 test cases passed"
         ],
         "test results": {
           "Test 0": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 888 5579 5524 6475 6725 9915 3113 9490 8248 5304 246 9675 9152 6816 6239 6330 5693 2098 1316 6657 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 1": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 4386 3278 226 2463 3123 1528 3275 5423 9318 4479 3109 4460 1898 9700 1156 2669 3628 4542 5384 3327 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 2": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 3114 2783 6123 4128 2421 4869 8948 9995 7550 9644 7801 7087 8085 1929 8526 2781 3936 1776 9498 4227 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 3": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 4737 2594 627 9292 518 6283 9153 9444 5639 3490 9970 1904 7564 6135 1539 2752 4766 7919 6850 4004 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 4": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 837 6765 4232 3027 5635 4625 4530 8883 2306 3155 3163 3409 7370 6247 4680 7109 5114 8719 5500 4711 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 5": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 1613 5038 5266 3815 1170 1097 7700 3605 1682 8076 9868 7861 4595 2213 3776 1338 7610 8891 8166 9033 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 6": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 1806 7184 6494 9359 7471 866 3706 9076 443 2320 4249 2812 4439 8938 2222 7099 2434 1603 7919 3038 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 7": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 4743 1988 9654 9700 7330 947 2933 6260 2106 6013 8163 9883 6502 6162 6298 174 7680 9761 4426 2349 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 8": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 4274 9969 6923 5024 1731 6163 1069 972 9692 1676 8223 175 951 68 7894 7834 6473 5407 1408 3015 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 9": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR] wrong element when insert\\n\\nError when processing: 5854 2959 4540 9642 1269 8738 9003 3714 4613 7669 7169 9958 1243 6012 7782 6427 7107 8039 6460 7921 \\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           }
         }
       },
       "PathPlan_AStar": {
-        "status": "PENDING",
+        "status": "PASS",
         "comments": [
-          "0 out of 5 test cases passed"
+          "5 out of 5 test cases passed"
         ],
         "test results": {
           "Test 0": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 1": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 2": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 3": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           },
           "Test 4": {
-            "Status": "Failed",
-            "Stderr": "b'[ERROR]: Search time out\\n'"
+            "Status": "Succeed",
+            "Stderr": "b''"
           }
         }
       },
